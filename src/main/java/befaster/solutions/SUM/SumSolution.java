e0aae1db5c2c092d88ae2f6ed6283a51f41d2600
sbx_user1059 <sbx_user1059@ip-10-128-200-143.eu-west-2.compute.internal>
2018-12-14 18:36:06 +0000

    Fri Dec 14 18:36:06 UTC 2018

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -1,11 +1,25 @@
 package befaster.solutions.SUM;
 
 import befaster.runner.SolutionNotImplementedException;
+import sun.nio.ch.IOStatus;
 
 public class SumSolution {
 
     public int compute(int x, int y) {
+        if (checkInput(x))
+            throw new IllegalArgumentException("The first arg is invalid");
+
+        if (checkInput(y))
+            throw new IllegalArgumentException("The second arg is invalid");
+
         return x+y;
     }
 
+    private boolean checkInput(int x) {
+        if(x < 0 || x >= 100)
+            return  false;
+        return true;
+    }
+
 }
+
