2de3b5296e5f9f972a6839f0ceb7211ca214fd9d
sbx_user1060 <sbx_user1060@ip-10-129-58-120.eu-west-2.compute.internal>
2018-12-14 19:48:31 +0000

    Fri Dec 14 19:48:31 UTC 2018

---
+++ src/test/java/befaster/solutions/FIZ/FIZTest.java
@@ -0,0 +1,20 @@
+package befaster.solutions.FIZ;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class FIZTest {
+
+    private FizzBuzzSolution fiz;
+
+    @Before
+    public void setUp() {
+
+        fiz = new FizzBuzzSolution();
+    }
+
+
+}
