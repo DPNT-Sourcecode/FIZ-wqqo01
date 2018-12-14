2ba98a073dac3f4b5bb8965f59f767a42b99bc8f
sbx_user1060 <sbx_user1060@ip-10-128-8-196.eu-west-2.compute.internal>
2018-12-14 20:17:08 +0000

    Fri Dec 14 20:17:08 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -32,7 +32,6 @@ public class FizzBuzzSolution {
     }
 
     private boolean multipleOf(Integer givenNumber, Integer multipleOf) {
-
         if((givenNumber % multipleOf) == 0 )
             return true;
 
@@ -40,7 +39,6 @@ public class FizzBuzzSolution {
     }
 
     private boolean containsValOf(Integer givenNumber, Integer valOf) {
-
         String numStr = Integer.toString(givenNumber);
         char charToCheck = Integer.toString(valOf).charAt(0);
         boolean result = false;
@@ -54,6 +52,25 @@ public class FizzBuzzSolution {
         return result;
     }
 
+    private boolean isDeluxe(Integer givenNumber) {
+        boolean result = true;
+        if (givenNumber < 10)
+            return false;
+
+        String numStr = Integer.toString(givenNumber);
+        char charToCheck = numStr.charAt(0);
+
+        for(int i = 0; i < numStr.length(); i++) {
+            if( numStr.charAt(i) != charToCheck) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
 
 }
+
 
